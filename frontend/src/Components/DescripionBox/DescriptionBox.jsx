cb6f085adfb477df592b5a3fcca26fc2cbbbc2df
iamcrozier <crozierservice@gmail.com>
2024-08-04 13:39:56 +0530

    fixed rating and description info

--- frontend/src/Components/DescripionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescripionBox/DescriptionBox.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import "./DescriptionBox.css";
 
-const DescriptionBox = () => {
+const DescriptionBox = (props) => {
   return (
     <div className="descriptionbox">
       <div className="descriptionbox-navigator">
         <div className="descriptionbox-nav-box">Description</div>
-        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        <div className="descriptionbox-nav-box fade">Reviews (0)</div>
       </div>
       <div className="descriptionbox-description">
         <p>
-          An e commerce website is an online platform that facilitate e buying
-          and selling of products or service over the internet serves as a
-          virtual marketplace where businesses and individuals showcase their
-          products, interact with customers, and conduct transactions without
-          the need for a physical presence. E-Commerce websites have gained
-          immence popularity due to their convenienc accessibility, and the
-          global reach they offer.
+          {props.product.description}
+          
         </p>
-        <p>
-
-        </p>
+        <p></p>
       </div>
     </div>
   );
